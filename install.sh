7a24cf75e4627d878b216270e046306f238c9e6e
Steven Haber <steven@qumulo.com>
2026-02-25 10:50:44 -0800

    claude

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ CWD=$(pwd)
 find "$CWD" -maxdepth 1 -type f ! -name install.sh -exec ln -sf {} ~/ \;
 ln -sf "$CWD"/.ssh/config ~/.ssh/
 
+mkdir -p ~/.claude
+ln -sf "$CWD"/.claude/keybindings.json ~/.claude/
+ln -sf "$CWD"/.claude/settings.json ~/.claude/
+
 crontab ~/.crontab
 
 sudo apt -y install emacs zile mosh
