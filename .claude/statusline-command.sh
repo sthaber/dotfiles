93a544df3f330687865f830c12f6a64c93460f46
Steven Haber <steven@qumulo.com>
2026-06-18 09:13:02 -0700

    Statusline improvements.

--- .claude/statusline-command.sh
+++ .claude/statusline-command.sh
@@ -5,6 +5,7 @@ used=$(echo "$input" | jq -r '.context_window.used_percentage // empty')
 five_h=$(echo "$input" | jq -r '.rate_limits.five_hour.used_percentage // empty')
 five_h_resets=$(echo "$input" | jq -r '.rate_limits.five_hour.resets_at // empty')
 week=$(echo "$input" | jq -r '.rate_limits.seven_day.used_percentage // empty')
+week_resets=$(echo "$input" | jq -r '.rate_limits.seven_day.resets_at // empty')
 version=$(echo "$input" | jq -r '.version // empty')
 model=$(echo "$input" | jq -r '.model.display_name // empty')
 
@@ -12,6 +13,26 @@ version_str=""
 if [ -n "$version" ]; then
     version_str=" (v${version})"
 fi
+
+# Render " resets XhYm" / " resets Ym" for a resets_at epoch, empty if past/unset.
+resets_str() {
+    local resets_at="$1"
+    if [ -z "$resets_at" ]; then
+        return
+    fi
+    local secs_left=$(( resets_at - $(date +%s) ))
+    if [ "$secs_left" -le 0 ]; then
+        return
+    fi
+    local mins_left=$(( secs_left / 60 ))
+    if [ "$mins_left" -ge 1440 ]; then
+        printf ' resets %dd%dh' "$(( mins_left / 1440 ))" "$(( mins_left % 1440 / 60 ))"
+    elif [ "$mins_left" -ge 60 ]; then
+        printf ' resets %dh%dm' "$(( mins_left / 60 ))" "$(( mins_left % 60 ))"
+    else
+        printf ' resets %dm' "$mins_left"
+    fi
+}
 
 bits=""
 if [ -n "$model" ]; then
@@ -22,26 +43,12 @@ if [ -n "$used" ]; then
 fi
 if [ -n "$five_h" ]; then
     five_h_int=$(printf '%.0f' "$five_h")
-    five_h_str="5h: ${five_h_int}%"
-    if [ -n "$five_h_resets" ]; then
-        now=$(date +%s)
-        secs_left=$(( five_h_resets - now ))
-        if [ "$secs_left" -gt 0 ]; then
-            mins_left=$(( secs_left / 60 ))
-            if [ "$mins_left" -ge 60 ]; then
-                hrs=$(( mins_left / 60 ))
-                mins=$(( mins_left % 60 ))
-                five_h_str="${five_h_str} resets ${hrs}h${mins}m"
-            else
-                five_h_str="${five_h_str} resets ${mins_left}m"
-            fi
-        fi
-    fi
-    bits="${bits} [${five_h_str}]"
+    bits="${bits} [5h: ${five_h_int}%$(resets_str "$five_h_resets")]"
 fi
 if [ -n "$week" ]; then
     week_int=$(printf '%.0f' "$week")
-    bits="${bits} [wk: ${week_int}%]"
+    bits="${bits} [wk: ${week_int}%$(resets_str "$week_resets")]"
 fi
 
-printf '\033[01;32m%s@%s\033[00m:\033[01;34m%s\033[00m%s%s' "$(whoami)" "$(hostname -s)" "$cwd" "$bits" "$version_str"
+dir="${cwd/#$HOME/\~}"
+printf '\033[01;34m%s\033[00m%s%s' "$dir" "$bits" "$version_str"
